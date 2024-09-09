9d7cebdd3367bf50c84615628c89fc5ee1212d06
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-09-09 21:43:23 +0300

    add rust-analyzer.toml to format leptos files

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,34 @@
 use leptos::*;
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> });
+    mount_to_body(|| view! { <App /> });
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <div style="display: flex">
+            <button
+                on:click=move |_| {
+                    set_count.update(|n| *n += 1);
+                }
+                class:red=move || count() % 2 == 1
+                style="position: absolute"
+                style:left=move || format!("{}px", count() + 100)
+                style:background-color=move || format!("rgb({}, {}, 100)", count(), 100)
+            >
+
+                "Click me: "
+                {move || count()}
+            </button>
+            <progress
+                max="50"
+                // signals are functions, so `value=count` and `value=move || count.get()`
+                // are interchangeable.
+                value=count
+            />
+        </div>
+    }
 }
