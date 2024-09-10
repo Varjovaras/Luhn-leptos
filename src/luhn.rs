d5aed9cf760515baefe1be03a9a0493208c404fb
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-09-10 12:35:36 +0300

    working luhn algorithm generator

--- src/luhn.rs
+++ src/luhn.rs
@@ -1,3 +1,60 @@
+use std::fmt::Display;
+
+use rand::Rng;
+
+const DIGITS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const LENGTH: u32 = 16;
+const ITERATIONS: u32 = 10_000_000;
+
+#[derive(Debug, Clone)]
+pub struct Luhn {
+    string_length: u32,
+    credit_card_number: String,
+}
+
+impl Luhn {
+    #[must_use]
+    pub fn new(s: &str) -> Self {
+        Self {
+            string_length: LENGTH,
+            credit_card_number: s.into(),
+        }
+    }
+
+    #[must_use]
+    pub fn generate_valid_luhn_number(&mut self) -> Self {
+        let mut rng = rand::thread_rng();
+        // let mut valid_count = 0;
+        let mut valid_number: Self = Self::new("");
+
+        for i in 0..ITERATIONS {
+            let random_string: String = (0..self.string_length)
+                .map(|_| DIGITS[rng.gen_range(0..DIGITS.len())])
+                .collect();
+
+            if is_credit_card(&random_string) {
+                println!("i = {i}");
+                valid_number.credit_card_number = random_string;
+                break;
+            }
+            // if i % 10_000 == 0 {
+            //     println!("i = {i}");
+            //     println!("valid numbers = {valid_count}");
+            // }
+        }
+        valid_number
+    }
+}
+
+impl Display for Luhn {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({})", self.credit_card_number)
+    }
+}
+
+#[must_use]
+#[allow(clippy::missing_panics_doc)]
+#[allow(clippy::unwrap_used)]
 pub fn is_credit_card(s: &str) -> bool {
     let chars: Vec<char> = s.chars().collect();
     let mut modified_string = String::new();
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[allow(clippy::wildcard_imports)]
 use leptos::*;
+use luhn::Luhn;
 
-mod luhn;
+pub mod luhn;
 
 fn main() {
     mount_to_body(|| view! { <App /> });
@@ -9,23 +10,25 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-
+    let new_luhn = Luhn::new("");
+    let (luhn, set_luhn) = create_signal(Luhn::new(""));
+    let generate_valid_luhn_number = luhn().generate_valid_luhn_number();
+    set_luhn(generate_valid_luhn_number);
     view! {
         <div style="flex ">
             <button on:click=move |_| {
-                set_count.update(|n| *n += 1);
+                set_luhn(luhn().generate_valid_luhn_number());
             }>
 
-                "Click me: " {move || count()}
+                "Click me: " {move || luhn().to_string()}
             </button>
-            <progress
-                class="p-16 w-full"
-                max="50"
-                // signals are functions, so `value=count` and `value=move || count.get()`
-                // are interchangeable.
-                value=count
-            />
+            // <progress
+            //     class="p-16 w-full"
+            //     max="50"
+            //     // signals are functions, so `value=count` and `value=move || count.get()`
+            //     // are interchangeable.
+            //     value=count
+            // />
         </div>
     }
 }
